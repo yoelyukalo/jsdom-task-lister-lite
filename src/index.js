875aeb2298f3c6cfefa4687376de1f05434e8cba
yoelyukalo <yoelyukalo@gmail.com>
2021-01-12 15:01:04 -0600

    doen

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const newForm = document.getElementById("create-task-form")
+  const newTaskDescription = document.getElementById("new-task-description");
+  
+  const newTaskUl = document.getElementById("tasks");
+
+
+  newForm.addEventListener("submit", createNewTask)
+
 });
+
+const createNewTask = event => {
+  event.preventDefault();
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+
+  newTask.innerText = newTaskDescription.value; 
+  appendNewTask(newTask);
+
+  event.target.reset();
+
+}
+
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+}
